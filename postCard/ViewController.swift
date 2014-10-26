8952f7ece2266761351f5d11b74b237d101a085d
Hitesh Sancheti <hitesh99@gmail.com>
2014-10-26 15:45:50 -0400

    Initial Commit

---
+++ postCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  postCard
+//
+//  Created by Hitesh Sancheti on 26/10/14.
+//  Copyright (c) 2014 Hitesh Sancheti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
